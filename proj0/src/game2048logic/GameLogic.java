6be1e30deffaf01aea9454523e998476e46f435c
fa_555 <fa_555@foxmail.com>
2025-07-19 01:27:29 +0800

    proj0: task 7 & finish

--- proj0/src/game2048logic/GameLogic.java
+++ proj0/src/game2048logic/GameLogic.java
@@ -1,22 +1,24 @@
 package game2048logic;
 
 import game2048rendering.Side;
+
 import static game2048logic.MatrixUtils.rotateLeft;
 import static game2048logic.MatrixUtils.rotateRight;
 
 /**
- * @author  Josh Hug
+ * @author Josh Hug
  */
 public class GameLogic {
-    /** Moves the given tile up as far as possible, subject to the minR constraint.
+    /**
+     * Moves the given tile up as far as possible, subject to the minR constraint.
      *
      * @param board the current state of the board
      * @param r     the row number of the tile to move up
-     * @param c -   the column number of the tile to move up
+     * @param c     -   the column number of the tile to move up
      * @param minR  the minimum row number that the tile can land in, e.g.
      *              if minR is 2, the moving tile should move no higher than row 2.
-     * @return      if there is a merge, returns the 1 + the row number where the merge occurred.
-     *              if no merge occurs, then return minR.
+     * @return if there is a merge, returns the 1 + the row number where the merge occurred.
+     * if no merge occurs, then return minR.
      */
     public static int moveTileUpAsFarAsPossible(int[][] board, int row, int column, int minR) {
         // DONE: Fill this in in tasks 2, 3, 4
@@ -47,8 +49,8 @@ public class GameLogic {
      * Modifies the board to simulate the process of tilting column c
      * upwards.
      *
-     * @param board     the current state of the board
-     * @param c         the column to tilt up.
+     * @param board the current state of the board
+     * @param c     the column to tilt up.
      */
     public static void tiltColumn(int[][] board, int c) {
         // DONE: fill this in in task 5
@@ -61,7 +63,7 @@ public class GameLogic {
     /**
      * Modifies the board to simulate tilting all columns upwards.
      *
-     * @param board     the current state of the board.
+     * @param board the current state of the board.
      */
     public static void tiltUp(int[][] board) {
         // DONE: fill this in in task 6
@@ -78,17 +80,31 @@ public class GameLogic {
      * @param side  the direction to tilt
      */
     public static void tilt(int[][] board, Side side) {
-        // TODO: fill this in in task 7
-        if (side == Side.NORTH) {
-            return;
-        } else if (side == Side.EAST) {
-            return;
-        } else if (side == Side.SOUTH) {
-            return;
-        } else if (side == Side.WEST) {
-            return;
-        } else {
-            System.out.println("Invalid side specified");
-        }
+        // DONE: fill this in in task 7
+        Runnable prework = () -> {
+            switch (side) {
+                case EAST -> rotateLeft(board);
+                case WEST -> rotateRight(board);
+                case SOUTH -> {
+                    rotateLeft(board);
+                    rotateLeft(board);
+                }
+            }
+        };
+
+        Runnable postwork = () -> {
+            switch (side) {
+                case EAST -> rotateRight(board);
+                case WEST -> rotateLeft(board);
+                case SOUTH -> {
+                    rotateRight(board);
+                    rotateRight(board);
+                }
+            }
+        };
+
+        prework.run();
+        tiltUp(board);
+        postwork.run();
     }
 }
