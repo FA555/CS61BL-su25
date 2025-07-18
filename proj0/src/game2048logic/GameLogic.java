3e59f38d589f662afeb07296085b8861a9c5b40b
fa_555 <fa_555@foxmail.com>
2025-07-19 01:23:33 +0800

    proj0: task 6

--- proj0/src/game2048logic/GameLogic.java
+++ proj0/src/game2048logic/GameLogic.java
@@ -64,8 +64,10 @@ public class GameLogic {
      * @param board     the current state of the board.
      */
     public static void tiltUp(int[][] board) {
-        // TODO: fill this in in task 6
-        return;
+        // DONE: fill this in in task 6
+        for (int c = 0; c < board[0].length; ++c) {
+            tiltColumn(board, c);
+        }
     }
 
     /**
