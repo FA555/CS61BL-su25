e6782c7e3d1617f6a7b2898e9798dd7c7a75c5eb
fa_555 <fa_555@foxmail.com>
2025-07-20 15:39:25 +0800

    [lab07] add: boolean set

--- lab07/src/BooleanSet.java
+++ lab07/src/BooleanSet.java
@@ -16,13 +16,25 @@ public class BooleanSet implements SimpleSet {
     /** Adds k to the set. */
     @Override
     public void add(int k) {
-        // TODO: Implement this method.
+        // DONE: Implement this method.
+        if (contains(k)) {
+            return;
+        }
+
+        contains[k] = true;
+        ++size;
     }
 
     /** Removes k from the set. */
     @Override
     public void remove(int k) {
-        // TODO: Implement this method.
+        // DONE: Implement this method.
+        if (!contains(k)) {
+            return;
+        }
+
+        contains[k] = false;
+        --size;
     }
 
     /** Return true if k is in this set, false otherwise. */
@@ -41,14 +53,21 @@ public class BooleanSet implements SimpleSet {
     /** Returns the number of items in the set. */
     @Override
     public int size() {
-        // TODO: Implement this method.
-        return 0;
+        // DONE: Implement this method.
+        return size;
     }
 
     /** Returns an array containing all of the elements in this collection. */
     @Override
     public int[] toIntArray() {
-        // TODO: Implement this method.
-        return null;
+        // DONE: Implement this method.
+        int[] result = new int[size];
+        int index = 0;
+        for (int i = 0; i < contains.length; i++) {
+            if (contains[i]) {
+                result[index++] = i;
+            }
+        }
+        return result;
     }
 }
--- lab07/test/BooleanSetTest.java
+++ lab07/test/BooleanSetTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 public class BooleanSetTest {
@@ -10,16 +11,43 @@ public class BooleanSetTest {
         assertWithMessage("Size is not zero upon instantiation").that(aSet.size()).isEqualTo(0);
         for (int i = 0; i < 100; i += 2) {
             aSet.add(i);
-            assertWithMessage("aSet should contain " + i).that(aSet.contains(i));
+            assertWithMessage("aSet should contain " + i).that(aSet.contains(i)).isTrue();
         }
 
         assertWithMessage("Size is not 50 after 50 calls to add").that(aSet.size()).isEqualTo(50);
         for (int i = 0; i < 100; i += 2) {
             aSet.remove(i);
-            assertWithMessage("aSet should not contain " + i).that(!aSet.contains(i));;
+            assertWithMessage("aSet should not contain " + i).that(!aSet.contains(i)).isTrue();
         }
 
-        assertWithMessage("aSet is not empty after removing all elements").that(aSet.isEmpty());
+        assertWithMessage("aSet is not empty after removing all elements").that(aSet.isEmpty()).isTrue();
         assertWithMessage("Size is not zero after removing all elements").that(aSet.size()).isEqualTo(0);
     }
+
+    @Test
+    public void testIntegrated() {
+        BooleanSet set = new BooleanSet(100);
+        assertWithMessage("Set should be empty").that(set.isEmpty()).isTrue();
+        assertWithMessage("Set size should be 0").that(set.size()).isEqualTo(0);
+
+        for (int i = 1; i < 100; i++) {
+            set.add(i);
+            assertWithMessage("Set should contain " + i).that(set.contains(i)).isTrue();
+            assertWithMessage("Set size should be " + i).that(set.size()).isEqualTo(i);
+        }
+
+        for (int i = 1; i < 100; i++) {
+            set.remove(i);
+            assertWithMessage("Set should not contain " + i).that(!set.contains(i)).isTrue();
+            assertWithMessage("Set size should be " + (100 - i - 1)).that(set.size()).isEqualTo(100 - i - 1);
+        }
+
+        set.add(1);
+        set.add(1);
+        set.add(4);
+        assertWithMessage("Set should contain 1 after adding it multiple times").that(set.contains(1)).isTrue();
+
+        assertThat(set.toIntArray()).asList().containsExactly(1, 4);
+    }
+
 }
