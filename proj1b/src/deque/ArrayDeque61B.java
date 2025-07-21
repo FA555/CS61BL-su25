9e0f72a6b32d979d6075731eac0ea6fdd5404ac9
fa_555 <fa_555@foxmail.com>
2025-07-21 18:36:54 +0800

    [proj1b] add: array deque

---
+++ proj1b/src/deque/ArrayDeque61B.java
@@ -0,0 +1,178 @@
+package deque;
+
+import javax.annotation.Nonnull;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+    private static final int INITIAL_CAPACITY = 8;
+    private static final int RESIZE_FACTOR = 2;
+    private static final double SHRINK_FACTOR = 0.25;
+
+    private T[] data;
+    private int size = 0;
+    private int nextFirst = 0;
+    private int nextLast = 1;
+
+    @SuppressWarnings("unchecked")
+    public ArrayDeque61B() {
+        data = (T[]) new Object[INITIAL_CAPACITY];
+    }
+
+    public int capacity() {
+        return data.length;
+    }
+
+    private void resize(int newCapacity) {
+        @SuppressWarnings("unchecked")
+        T[] newData = (T[]) new Object[newCapacity];
+
+        for (int i = 0; i < size; ++i) {
+            newData[i] = data[(nextFirst + 1 + i) % capacity()];
+        }
+        data = newData;
+        nextFirst = newCapacity - 1;
+        nextLast = size;
+    }
+
+    private void checkResize() {
+        if (size == capacity()) {
+            resize(size * RESIZE_FACTOR);
+        } else if (size < capacity() * SHRINK_FACTOR && capacity() > INITIAL_CAPACITY) {
+            resize(size * RESIZE_FACTOR);
+        }
+    }
+
+    private int indexOffset(int index, int offset) {
+        return (index + offset + capacity()) % capacity();
+    }
+
+    private int indexInc(int index) {
+        return indexOffset(index, 1);
+    }
+
+    private int indexDec(int index) {
+        return indexOffset(index, -1);
+    }
+
+    @Override
+    public void addFirst(T x) {
+        checkResize();
+        data[nextFirst] = x;
+        nextFirst = indexDec(nextFirst);
+        ++size;
+    }
+
+    @Override
+    public void addLast(T x) {
+        checkResize();
+        data[nextLast] = x;
+        nextLast = indexInc(nextLast);
+        ++size;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> list = new java.util.ArrayList<>(size);
+        for (int i = indexInc(nextFirst); i != nextLast; i = indexInc(i)) {
+            list.add(data[i]);
+        }
+        return list;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        nextFirst = indexInc(nextFirst);
+        --size;
+        T removedItem = data[nextFirst];
+        data[nextFirst] = null;
+        checkResize();
+        return removedItem;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        nextLast = indexDec(nextLast);
+        --size;
+        T removedItem = data[nextLast];
+        data[nextLast] = null;
+        checkResize();
+        return removedItem;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return data[indexOffset(nextFirst, 1 + index)];
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for ArrayDeque61B");
+    }
+
+    class ArrayDequeIterator implements Iterator<T> {
+        private int currentIndex = indexInc(nextFirst);
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex != nextLast;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }
+
+            T item = data[currentIndex];
+            currentIndex = indexInc(currentIndex);
+            return item;
+        }
+    }
+
+    @Override
+    @Nonnull
+    public Iterator<T> iterator() {
+        return new ArrayDequeIterator();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Deque61B<?> other) || size != other.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (!get(i).equals(other.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return toList().toString();
+    }
+}
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -1,4 +1,5 @@
 import deque.ArrayDeque61B;
+import deque.Deque61B;
 
 import jh61b.utils.Reflection;
 import org.junit.jupiter.api.DisplayName;
@@ -12,14 +13,193 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
-//     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
-//
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-//     }
+    @Test
+    @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+    void noNonTrivialFields() {
+        List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                .toList();
+
+        assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+    }
+
+    @Test
+    public void testEqualDeques61B() {
+        Deque61B<String> lld1 = new ArrayDeque61B<>();
+        Deque61B<String> lld2 = new ArrayDeque61B<>();
+
+        lld1.addLast("front");
+        lld1.addLast("middle");
+        lld1.addLast("back");
+
+        lld2.addLast("front");
+        lld2.addLast("middle");
+        lld2.addLast("back");
+
+        assertThat(lld1).isEqualTo(lld2);
+    }
+
+    @Test
+    public void addFirstTestBasic() {
+        Deque61B<String> lld1 = new ArrayDeque61B<>();
+
+        lld1.addFirst("back");
+        assertThat(lld1.toList()).containsExactly("back").inOrder();
+
+        lld1.addFirst("middle");
+        assertThat(lld1.toList()).containsExactly("middle", "back").inOrder();
+
+        lld1.addFirst("front");
+        assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+    }
+
+    @Test
+    public void addLastTestBasic() {
+        Deque61B<String> lld1 = new ArrayDeque61B<>();
+
+        lld1.addLast("front");
+        lld1.addLast("middle");
+        lld1.addLast("back");
+        assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+    }
+
+    @Test
+    public void addFirstAndAddLastTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+
+        lld1.addLast(0);
+        lld1.addLast(1);
+        lld1.addFirst(-1);
+        lld1.addLast(2);
+        lld1.addFirst(-2);
+
+        assertThat(lld1.toList()).containsExactly(-2, -1, 0, 1, 2).inOrder();
+    }
+
+    @Test
+    public void removeFirstTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.removeFirst()).isNull();
+
+        lld1.addFirst(114);
+        assertThat(lld1.removeFirst()).isEqualTo(114);
+        assertThat(lld1.toList()).isEmpty();
+
+        lld1.addLast(-514);
+        lld1.addLast(1919);
+        lld1.addFirst(810);
+        assertThat(lld1.removeFirst()).isEqualTo(810);
+        assertThat(lld1.toList()).containsExactly(-514, 1919).inOrder();
+    }
+
+    @Test
+    public void removeLastTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.removeLast()).isNull();
+
+        lld1.addFirst(114);
+        assertThat(lld1.removeLast()).isEqualTo(114);
+        assertThat(lld1.toList()).isEmpty();
+
+        lld1.addLast(-514);
+        lld1.addLast(1919);
+        lld1.addFirst(810);
+        assertThat(lld1.removeLast()).isEqualTo(1919);
+        assertThat(lld1.toList()).containsExactly(810, -514).inOrder();
+    }
+
+    @Test
+    public void isEmptyTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.isEmpty()).isTrue();
+
+        lld1.addFirst(114);
+        assertThat(lld1.isEmpty()).isFalse();
+
+        lld1.removeFirst();
+        assertThat(lld1.isEmpty()).isTrue();
+
+        lld1.addLast(-514);
+        assertThat(lld1.isEmpty()).isFalse();
+
+        lld1.addFirst(-1919);
+        assertThat(lld1.isEmpty()).isFalse();
+    }
+
+    @Test
+    public void sizeTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.size()).isEqualTo(0);
+
+        lld1.addFirst(114);
+        assertThat(lld1.size()).isEqualTo(1);
+
+        lld1.addLast(-514);
+        assertThat(lld1.size()).isEqualTo(2);
+
+        lld1.removeFirst();
+        assertThat(lld1.size()).isEqualTo(1);
+
+        lld1.removeLast();
+        assertThat(lld1.size()).isEqualTo(0);
+
+        lld1.addLast(1919);
+        lld1.addFirst(810);
+        assertThat(lld1.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void getTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.get(0)).isNull();
+
+        lld1.addFirst(114);
+        assertThat(lld1.get(0)).isEqualTo(114);
+        assertThat(lld1.get(1)).isNull();
+
+        lld1.addLast(-514);
+        lld1.addLast(1919);
+        lld1.addFirst(810);
+        assertThat(lld1.get(0)).isEqualTo(810);
+        assertThat(lld1.get(1)).isEqualTo(114);
+        assertThat(lld1.get(2)).isEqualTo(-514);
+        assertThat(lld1.get(3)).isEqualTo(1919);
+        assertThat(lld1.get(4)).isNull();
+
+        assertThat(lld1.get(114514)).isNull();
+        assertThat(lld1.get(-1919810)).isNull();
+    }
+
+    @Test
+    public void integrationTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        assertThat(lld1.isEmpty()).isTrue();
+        assertThat(lld1.size()).isEqualTo(0);
+        assertThat(lld1.toList()).isEmpty();
+
+        lld1.addFirst(1);
+        assertThat(lld1.isEmpty()).isFalse();
+        assertThat(lld1.size()).isEqualTo(1);
+        assertThat(lld1.toList()).containsExactly(1).inOrder();
+
+        lld1.addLast(2);
+        assertThat(lld1.isEmpty()).isFalse();
+        assertThat(lld1.size()).isEqualTo(2);
+        assertThat(lld1.toList()).containsExactly(1, 2).inOrder();
+
+        lld1.removeFirst();
+        assertThat(lld1.isEmpty()).isFalse();
+        assertThat(lld1.size()).isEqualTo(1);
+        assertThat(lld1.toList()).containsExactly(2).inOrder();
+
+        lld1.removeLast();
+        assertThat(lld1.isEmpty()).isTrue();
+        assertThat(lld1.size()).isEqualTo(0);
+        assertThat(lld1.toList()).isEmpty();
+
+        lld1.addLast(3);
+        lld1.addLast(4);
+        assertThat(lld1.get(0)).isEqualTo(3);
+    }
 
 }
